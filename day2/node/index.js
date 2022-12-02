e0f647a1bfa617ccd7c0c76b81922734679a8365
Ricardo Rocha <me@rrocha.uk>
2022-12-02 14:00:02 +0000

    * second challenge of day2;

--- day2/node/index.js
+++ day2/node/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
-const maps = {
-  'A X': 4,
+const f = {
+  'A X': 7,
   'A Y': 8,
   'A Z': 3,
   'B X': 1,
@@ -12,10 +12,25 @@ const maps = {
   'C Z': 6,
 }
 
-let total = 0;
+const s = {
+  'A X': 3,
+  'A Y': 4,
+  'A Z': 8,
+  'B X': 1,
+  'B Y': 5,
+  'B Z': 9,
+  'C X': 2,
+  'C Y': 6,
+  'C Z': 7,
+}
+
+let totalF = 0;
+let totalS = 0;
 fs.readFile('../inputs.txt', (_, data) => {
   data.toString().split('\n').map(l => {
-    total += maps[l];
+    totalF += f[l];
+    totalS += s[l];
   });
-  console.log('total points:', total);
+  console.log('total points:', totalF);
+  console.log('total points:', totalS);
 });
