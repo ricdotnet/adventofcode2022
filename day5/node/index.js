64328eeb3c88ca0742b3c6a59bc4907dc0aca1db
Ricardo Rocha <me@rrocha.uk>
2022-12-05 21:15:36 +0000

    * day5 refactor... added a data manipulator;

--- day5/node/index.js
+++ day5/node/index.js
@@ -1,25 +1,37 @@
 const fs = require('fs');
 
-const final = [[], []];
-
 let [groups, steps] = fs.readFileSync('../inputs.txt')
   .toString()
   .split('\n\n')
   .map(x => x.split('\n'));
-let groups2 = [...groups];
 
-groups = groups.map(x => x.split(''));
-groups2 = groups2.map(x => x.split(''));
+const stacks = groups.map(x => {
+  return x.replace(/    /g, '.')
+    .replace(/ \[/g, '')
+    .replace(/] /g, '')
+    .replace(/\[/g, '')
+    .replace(/]/g, '');
+})
+  .map(x => x.replaceAll(' ', ''))
+  .map(x => x.split(''));
 
-steps.map(x => {
-  const n = x.match(/\d+/g)
-  const p = groups[n[1] - 1].slice(-n[0]).reverse();
-  const p2 = groups2[n[1] - 1].slice(-n[0]);
-  groups[n[1] - 1].splice(-n[0]);
-  groups2[n[1] - 1].splice(-n[0]);
-  groups[n[2] - 1].push(...p);
-  groups2[n[2] - 1].push(...p2);
+const stacks1 = Array.from(Array(stacks[stacks.length -1 ].length), () => []);
+
+stacks.slice(0, -1).map((x) => {
+  x.map((y, i) => (y !== '.') ? stacks1[i].unshift(y) : null);
 });
 
-console.log(groups.map(x => x[x.length - 1]).join(''));
-console.log(groups2.map(x => x[x.length - 1]).join(''));
+const stacks2 = stacks1.map(x => [...x]);
+
+steps.slice(0, -1).map(x => {
+  const n = x.match(/\d+/g);
+  const p = stacks1[n[1] - 1].slice(-n[0]).reverse();
+  const p2 = stacks2[n[1] - 1].slice(-n[0]);
+  stacks1[n[1] - 1].splice(-n[0]);
+  stacks2[n[1] - 1].splice(-n[0]);
+  stacks1[n[2] - 1].push(...p);
+  stacks2[n[2] - 1].push(...p2);
+});
+
+console.log(stacks1.map(x => x[x.length - 1]).join(''));
+console.log(stacks2.map(x => x[x.length - 1]).join(''));
