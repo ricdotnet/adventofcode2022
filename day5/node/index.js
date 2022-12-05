2b1e9d58c9f3242fc2844c89903f3e866fca2656
Ricardo Rocha <me@rrocha.uk>
2022-12-05 10:00:20 +0000

    * day5 nodejs;

--- day5/node/index.js
+++ day5/node/index.js
@@ -1,3 +1,27 @@
 const fs = require('fs');
 
-fs.readFileSync('../inputs.test').toString().split('\n\n').map(x => console.log(x));
+const final = [[], []];
+
+let [groups, steps] = fs.readFileSync('../inputs.txt')
+  .toString()
+  .split('\n\n')
+  .map(x => x.split('\n'));
+let groups2 = [...groups];
+
+groups = groups.map(x => x.split(''));
+groups2 = groups2.map(x => x.split(''));
+
+steps.map(x => {
+  const n = x.match(/\d+/g)
+  const p = groups[n[1] - 1].slice(-n[0]).reverse();
+  const p2 = groups2[n[1] - 1].slice(-n[0]);
+  groups[n[1] - 1].splice(-n[0]);
+  groups2[n[1] - 1].splice(-n[0]);
+  groups[n[2] - 1].push(...p);
+  groups2[n[2] - 1].push(...p2);
+});
+
+groups.map(x => final[0].push(x[x.length - 1]));
+console.log(final[0].join(''));
+groups2.map(x => final[1].push(x[x.length - 1]));
+console.log(final[1].join(''));
