10e46ac286694ed4305cfdc343dc67a95ebaa4b8
Ricardo Rocha <me@rrocha.uk>
2022-12-08 14:36:51 +0000

    * day8 part2;

--- day8/node/index.js
+++ day8/node/index.js
@@ -4,6 +4,7 @@ const treeMap = fs.readFileSync('../inputs.txt').toString().split('\n').slice(0,
 const outerRing = ((treeMap[0].length-1) * 2) + ((treeMap.length * 2) - 2);
 
 let visible = 0;
+let scenicScores = [];
 
 for (let i = 1; i < treeMap.length - 1; i++) {
   for (let j = 1; j < treeMap[i].length - 1; j++) {
@@ -12,7 +13,45 @@ for (let i = 1; i < treeMap.length - 1; i++) {
       || checkAbove(tree, j, i) || checkBelow(tree, j, i)) {
       visible++;
     }
+    calculateScenicScores(tree, treeMap[i], j, j, i)
   }
+}
+
+function calculateScenicScores(tree, row, col, currX, currY) {
+  let l = 0, r = 0, a = 0, b = 0;
+  for (let i = currX - 1; i >= 0; i--) {
+    if (+row[i] < +tree) {
+      l++;
+    } else if (+row[i] >= +tree) {
+      l++;
+      break;
+    }
+  }
+  for (let w = currX + 1; w < row.length; w++) {
+    if (+row[w] < +tree) {
+      r++;
+    } else if (+row[w] >= +tree) {
+      r++;
+      break;
+    }
+  }
+  for (let i = currY - 1; i >= 0; i--) {
+    if (+treeMap[i][col] < +tree) {
+      a++;
+    } else if (+treeMap[i][col] >= +tree) {
+      a++;
+      break;
+    }
+  }
+  for (let i = currY + 1; i < treeMap.length; i++) {
+    if (+treeMap[i][col] < +tree) {
+      b++;
+    } else if (+treeMap[i][col] >= +tree) {
+      b++;
+      break;
+    }
+  }
+  scenicScores.push(l * r * a * b);
 }
 
 function checkLeft(tree, row, currY) {
@@ -35,9 +74,9 @@ function checkRight(tree, row, currY) {
   return isVisible;
 }
 
-function checkAbove(tree, col, row) {
+function checkAbove(tree, col, currX) {
   let isVisible = true;
-  for (let i = 0; i < row; i++) {
+  for (let i = 0; i < currX; i++) {
     if (+treeMap[i][col] >= +tree) {
       isVisible = false;
     }
@@ -45,9 +84,9 @@ function checkAbove(tree, col, row) {
   return isVisible;
 }
 
-function checkBelow(tree, col, row) {
+function checkBelow(tree, col, currX) {
   let isVisible = true;
-  for (let i = row + 1; i < treeMap.length; i++) {
+  for (let i = currX + 1; i < treeMap.length; i++) {
     if (+treeMap[i][col] >= +tree) {
       isVisible = false;
       break;
@@ -56,4 +95,5 @@ function checkBelow(tree, col, row) {
   return isVisible;
 }
 
-console.log(outerRing + visible);
+console.log('visible trees:', outerRing + visible);
+console.log('highest scenic score:', scenicScores.sort((a, b) => b - a)[0]);
